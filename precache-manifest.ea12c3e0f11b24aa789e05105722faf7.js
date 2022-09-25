0117bbc38d7ad71844e9a873be48d92c2fc8e886
vinnn <vmoutel@yahoo.fr>
2022-09-25 22:32:07 +0100

    Updates

---
+++ precache-manifest.ea12c3e0f11b24aa789e05105722faf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89b7fd6a5d213f474f11",
+    "url": "/ODIN_proj08_cv_hooks/static/css/main.4821eade.chunk.css"
+  },
+  {
+    "revision": "89b7fd6a5d213f474f11",
+    "url": "/ODIN_proj08_cv_hooks/static/js/main.89b7fd6a.chunk.js"
+  },
+  {
+    "revision": "05a8e767b6b683ee5dc9",
+    "url": "/ODIN_proj08_cv_hooks/static/js/1.05a8e767.chunk.js"
+  },
+  {
+    "revision": "0c51a45b2d27b8a76701",
+    "url": "/ODIN_proj08_cv_hooks/static/js/2.0c51a45b.chunk.js"
+  },
+  {
+    "revision": "e646dd22ad3c79645d6e",
+    "url": "/ODIN_proj08_cv_hooks/static/js/runtime~main.e646dd22.js"
+  },
+  {
+    "revision": "04eccd8f785cf2ba7f36ad0cbca9e3ac",
+    "url": "/ODIN_proj08_cv_hooks/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ODIN_cv_hooks/precache-manifest.9c1d26595519dbe2841881516ce770f6.js"
+  "/ODIN_proj08_cv_hooks/precache-manifest.ea12c3e0f11b24aa789e05105722faf7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ODIN_cv_hooks/index.html", {
+workbox.routing.registerNavigationRoute("/ODIN_proj08_cv_hooks/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
